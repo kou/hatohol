b37242fa7d0c6dc4ddc0a015a17d3d3925c7c02a
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-02-27 13:14:39 +0900

    added tests in testItemData.
    - test_getDouble
    - test_setDouble

--- server/test/testItemData.cc
+++ server/test/testItemData.cc
@@ -193,6 +193,26 @@ void test_getDouble(void)
 	assertGet(double, ItemDouble, 0.98);
 }
 
+void test_getDouble(void)
+{
+	double val = 0.98;
+	ItemData *item = new ItemDouble(val);
+	double readValue;
+	item->get(&readValue);
+	cppcut_assert_equal(val, readValue);
+}
+
+void test_setDouble(void)
+{
+	double val = -5.2;
+	ItemData *item = new ItemDouble(val);
+	val = -4.8;
+	double readValue;
+	item->set(&val);
+	item->get(&readValue);
+	cppcut_assert_equal(val, readValue);
+}
+
 void test_getString(void)
 {
 	assertGet(string, ItemString, "test String");
